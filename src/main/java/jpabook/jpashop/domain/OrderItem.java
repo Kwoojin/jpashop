cf8be3b140c104463268e28fa6caff923c8b5463
WoojinKim <dnwls488@naver.com>
2022-06-14 15:40:50 +0900

    OrderItem 생성 메서드 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,8 +25,31 @@ public class OrderItem {
     private int orderPrice; //주문가격
     private int count; //주문수량
 
+    protected void setItem(Item item) {
+        this.item = item;
+    }
+
+    protected void setOrderPrice(int orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    protected void setCount(int count) {
+        this.count = count;
+    }
+
     protected void setOrder(Order order) {
         this.order = order;
+    }
+
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
     }
 
     //==비즈니스 로직==/
@@ -34,6 +57,10 @@ public class OrderItem {
         item.addStock(count);
     }
 
+    //==조회 로직==//
+    /**
+     * 주문 상품 전체 가격 조회
+     */
     public int getTotalPrice() {
         return orderPrice * count;
     }
