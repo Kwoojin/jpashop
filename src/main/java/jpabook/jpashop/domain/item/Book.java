e38c343b1313cceddbb782b2a057fc866506e38e
WoojinKim <dnwls488@naver.com>
2022-06-15 19:32:47 +0900

    생성자 추가

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop.domain.item;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
@@ -9,8 +8,17 @@ import javax.persistence.Entity;
 @Entity
 @DiscriminatorValue("B")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class Book extends Item {
 
     private String author;
     private String isbn;
+
+    public Book(String name, int price, int stockQuantity, String author, String isbn) {
+        super(name, price, stockQuantity);
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
