56467772866d5dd9280b150a13f2d0ceb883e237
WoojinKim <dnwls488@naver.com>
2022-06-16 18:22:23 +0900

    아이템 수정 기능 추가

--- src/main/java/jpabook/jpashop/controller/ItemController.java
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -6,9 +6,7 @@ import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,4 +43,42 @@ public class ItemController {
         model.addAttribute("items", items);
         return "items/itemList";
     }
+
+    @GetMapping("/{itemId}/edit")
+    public String updateItemForm(@PathVariable("itemId") Long itemId, Model model) {
+        Book item = (Book) itemService.findItemOne(itemId);
+
+        BookForm form = new BookForm();
+        form.setId(item.getId());
+        form.setName(item.getName());
+        form.setPrice(item.getPrice());
+        form.setStockQuantity(item.getStockQuantity());
+        form.setAuthor(item.getAuthor());
+        form.setIsbn(item.getIsbn());
+
+        model.addAttribute("form", form);
+        return "items/updateItemForm";
+    }
+
+    /**
+     * form의 Id는 조작되어 넘어올 수 있기 때문에,
+     * 권한을 체크할 수 있는 방법이 있어야함
+     */
+    @PostMapping("/{itemId}/edit")
+    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form) {
+        Book book = new Book(
+                form.getId(),
+                form.getName(),
+                form.getPrice(),
+                form.getStockQuantity(),
+                form.getAuthor(),
+                form.getIsbn()
+        );
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+
+    }
+
+
 }
