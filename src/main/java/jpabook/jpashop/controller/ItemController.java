91c5470809d51a268cc969588096eb6c2f79ad28
WoojinKim <dnwls488@naver.com>
2022-06-16 17:06:30 +0900

    item 등록, 리스트 기능 추가

---
+++ src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,48 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/new")
+    public String create(BookForm bookForm) {
+        Book book = new Book(
+                bookForm.getName(),
+                bookForm.getPrice(),
+                bookForm.getStockQuantity(),
+                bookForm.getAuthor(),
+                bookForm.getIsbn()
+        );
+
+        itemService.saveItem(book);
+        return "redirect:/items";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+    }
+}
